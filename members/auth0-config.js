09d2f148114135b075cbc10e016402936b7598aa
Steven Edward Kittinger <skittingerjr@yahoo.com>
2025-12-30 21:54:55 +0800

    Update auth0-config.js

--- members/auth0-config.js
+++ members/auth0-config.js
@@ -1,5 +1,6 @@
+// members/auth0-config.js
 window.AUTH0_CONFIG = {
   domain: "defy.us.auth0.com",
-  clientId: kpDwkl80nI0qVbBmHf9ui2Tc2r1AjEVu,
+  clientId: "kpDwkl80nI0qVbBmHf9ui2Tc2r1AjEVu",
   redirectUri: window.location.origin + "/defy-elite-athletics/members/members-area.html"
 };
