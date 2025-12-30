0b4a69394ce42540a5ffb6d50fc06ed8b18686a7
Steven Edward Kittinger <skittingerjr@yahoo.com>
2025-12-30 21:33:32 +0800

    Add Auth0 configuration for authentication

---
+++ members/auth0-config.js
@@ -0,0 +1,5 @@
+window.AUTH0_CONFIG = {
+  domain: "defy.us.auth0.com",
+  clientId: "PASTE_YOUR_CLIENT_ID_HERE",
+  redirectUri: window.location.origin + "/defy-elite-athletics/members/members-area.html"
+};
